0968d45b207b14ce73ba8568471cffb072e5e04d
AnettaKo <an.kozhukhova@gmail.com>
2023-11-25 01:13:06 +0100

    test

--- main_interface.py
+++ main_interface.py
@@ -2,6 +2,4 @@ from my_functions.interface import main_menu
 
 if __name__ == '__main__':
 
-    # my_wardrobe = read_data()
-    # main_menu(my_wardrobe)
     main_menu()
---
+++ test.py
@@ -0,0 +1,64 @@
+from fastapi.testclient import TestClient
+from my_functions.api import app
+from my_functions.classes import Item
+
+test_client = TestClient(app)
+id = ""
+
+def test_app():
+
+    test_article = Item(id = "",
+                       item_name = "test",
+                       item_class = "closes",
+                       item_type = "pant",
+                       size = "104",
+                       season = "winter",
+                       color = "red",
+                       brand = "kik",
+                       price = 0.0,
+                       description = "")
+
+    test_article_update = Item(id="",
+                        item_name="test1",
+                        item_class="closes",
+                        item_type="pant",
+                        size="104",
+                        season="winter",
+                        color="red",
+                        brand="kik",
+                        price=0.0,
+                        description="")
+
+    r = test_client.post("/articles", json=test_article.model_dump())
+
+    assert r.status_code == 200
+
+    r = test_client.get("/articles/test")
+    assert r.status_code == 200
+    article = r.json()
+    id = article["_id"]
+
+    r = test_client.put("/articles/" + id, json = test_article_update.model_dump())
+    assert r.status_code == 200
+
+    r = test_client.get("/articles/test1")
+    assert r.status_code == 200
+    article = r.json()
+    assert article ["item_name"] == "test1"
+
+
+    r = test_client.get("/articles/test2")
+    assert r.status_code == 200
+    article = r.json()
+    assert article == None
+
+
+    r = test_client.delete("/articles/" + id)
+    assert r.status_code == 200
+
+    r = test_client.get("/articles/test1")
+    assert r.status_code == 200
+    article = r.json()
+    assert article == None
+
+test_app()
